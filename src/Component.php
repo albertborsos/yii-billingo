cd45838986375fc77a69452b5768faac57cf1b68
Albert Borsos <albertborsos@me.com>
2018-02-12 12:31:53 +0100

    add billingo api to component

--- src/Component.php
+++ src/Component.php
@@ -2,7 +2,33 @@
 
 namespace albertborsos\billingo;
 
+use Billingo\API\Connector\HTTP\Request;
+
 class Component extends \CComponent
 {
+    /**
+     * @var string
+     */
+    public $publicKey;
 
+    /**
+     * @var string
+     */
+    public $privateKey;
+
+    /**
+     * @var Request
+     */
+    private $_api;
+
+    public function getApi()
+    {
+        if (empty($this->_api)) {
+            $this->_api = new Request(array(
+                'publicKey' => $this->publicKey,
+                'privateKey' => $this->privateKey,
+            ));
+        }
+        return $this->_api;
+    }
 }
