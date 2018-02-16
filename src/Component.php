ba71b39de581af86b2e10a739d2a91f5d89d63e7
Albert Borsos <albertborsos@me.com>
2018-02-16 14:09:11 +0100

    add a workaround to filter invoices

--- src/Component.php
+++ src/Component.php
@@ -6,6 +6,8 @@ use Billingo\API\Connector\HTTP\Request;
 
 class Component extends \CComponent
 {
+    const MAX_PER_PAGE = 50;
+
     /**
      * @var string
      */
@@ -37,5 +39,32 @@ class Component extends \CComponent
     private function setApi(Request $api)
     {
         $this->_api = $api;
+    }
+
+    public function getInvoices($filters = [])
+    {
+        $page = 1;
+        $invoices = [];
+        do {
+            $response = $this->getApi()->get('invoices', [
+                'page' => $page,
+                'max_per_page' => self::MAX_PER_PAGE,
+            ]);
+
+            if (is_array($response)) {
+                $invoices = \CMap::mergeArray($invoices, $response);
+            }
+
+            $page++;
+        } while ($response !== null);
+
+        return array_filter($invoices, function ($item) use ($filters) {
+            foreach ($filters as $attribute => $value) {
+                if ($value == \ArrayHelper::getValue($item, 'attributes.' . $attribute)) {
+                    return true;
+                }
+            }
+            return false;
+        });
     }
 }
